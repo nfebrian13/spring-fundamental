d29847f05d254ef501e07b439824822d31495b14
nfebrian13 <febrian.nana@gmail.com>
2020-05-02 12:13:02 +0700

    call injection bean config

---
+++ SpringCore/src/main/java/com/core/spring/ApplicationContextMainClass.java
@@ -0,0 +1,23 @@
+package com.core.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.core.spring.model.Title;
+
+/** 
+ * Berikut adalah contoh untuk memanggil DI yang telah dikonfigurasi di xml
+ * 
+ * */
+
+public class ApplicationContextMainClass {
+
+	public static void main(String[] args) {
+		ApplicationContext springContainer = new ClassPathXmlApplicationContext("com/core/spring/config/beans.xml");
+		Title setterInjection = (Title) springContainer.getBean("bookTitle"); /* bookTitle => id object di xml beans */ 
+		Title constructorInjection = (Title) springContainer.getBean("chapter1Title");
+		
+		System.out.println(constructorInjection.getTitleValue());
+	}
+
+}
---
+++ SpringCore/src/main/java/com/core/spring/config/BeansConfiguration.java
@@ -0,0 +1,5 @@
+package com.core.spring.config;
+
+public class BeansConfiguration {
+
+}
