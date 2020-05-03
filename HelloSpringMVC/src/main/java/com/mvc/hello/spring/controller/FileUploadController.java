c72f7bbff51b021b93bc83b740c39634305663a8
nfebrian13 <febrian.nana@gmail.com>
2020-05-03 12:47:02 +0700

    add upload file controller and config

--- HelloSpringMVC/src/main/java/com/mvc/hello/spring/controller/FileUploadController.java
+++ HelloSpringMVC/src/main/java/com/mvc/hello/spring/controller/FileUploadController.java
@@ -1,40 +1,44 @@
 package com.mvc.hello.spring.controller;
 
-import java.io.IOException;
-import java.sql.SQLException;
+import java.io.File;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 @Controller
 public class FileUploadController {
 
 	private Logger logger = Logger.getLogger(FileUploadController.class);
 
+	private String saveDirectory = "E:/logs/upload/";
+	
 	@RequestMapping(value = "/uploadFile", method = RequestMethod.GET)
-	public String doFileUpload(@RequestParam int a) throws IOException, SQLException {
-
-		logger.info("This is an info log entry");
-		logger.error("This is an error log entry");
-		
-		// handles file upload stuff...
-		if (a == 1) {
-			throw new IOException("Could not read upload file.");
-		} else if (a == 2) {
-			throw new SQLException("Database exception!!!");
-		}
-		return "done";
+	public String test(Model model) {
+		return "uploadForm";
 	}
-
-	@ExceptionHandler({ IOException.class, java.sql.SQLException.class })
-	public ModelAndView handleIOException(Exception ex) {
-		ModelAndView model = new ModelAndView("IOError");
-		model.addObject("exception", ex.getMessage());
-		return model;
-	}
+	
+    @RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
+    public String handleFileUpload(HttpServletRequest request,
+            @RequestParam CommonsMultipartFile[] fileUpload) throws Exception {
+         
+    	logger.info("description: " + request.getParameter("description"));
+         
+        if (fileUpload != null && fileUpload.length > 0) {
+            for (CommonsMultipartFile aFile : fileUpload){
+            	logger.info("Saving file: " + aFile.getOriginalFilename());
+                if (!aFile.getOriginalFilename().equals("")) {
+                    aFile.transferTo(new File(saveDirectory + aFile.getOriginalFilename()));
+                }
+            }
+        }
+ 
+        return "result";
+    }
 }
