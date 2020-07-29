46f674d19dad3a6003c4fac91786a4fcbe74cc1e
nfebrian13 <nanaz.febrian@gmail.com>
2020-07-29 22:34:46 +0700

    initial commit

---
+++ springboot-thymeleaf-template/src/test/java/com/thymeleaf/springboot/SpringbootThymeleafTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thymeleaf.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootThymeleafTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
